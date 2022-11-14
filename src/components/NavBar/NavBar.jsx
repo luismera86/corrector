27ca57774ab2c0121ea162f91367a2391278ab27
Luis Mera <luismera86@gmail.com>
2022-11-14 12:19:02 -0300

    comentario 4 arreglado

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,37 +4,29 @@ import styles from './styles/NavBar.module.css'
 const NavBar = () => {
   return (
     <div className='oculto-impresion'>
-    <nav className={styles.navbar}>
-      <ul className={styles.ul}>
-        <li className={styles.li}>
+      <nav className={styles.navbar}>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
             <NavLink className={styles.a} to='#'>
-          <button className={styles.button}>
-          Home
-          </button>
+              <button className={styles.button}>Home</button>
             </NavLink>
-        </li>
-        <li className={styles.li}>
+          </li>
+          <li className={styles.li}>
             <NavLink className={styles.a} to='/delivery1'>
-          <button className={styles.button}>
-              Primera Entrega
-          </button>
+              <button className={styles.button}>Primera Entrega</button>
             </NavLink>
-        </li>
-        <li className={styles.li}>
+          </li>
+          <li className={styles.li}>
             <NavLink className={styles.a} to='/delivery2'>
-          <button className={styles.button}>
-              Segunda Entrega
-          </button>
+              <button className={styles.button}>Segunda Entrega</button>
             </NavLink>
-        </li>
-        <li className={styles.li}>
+          </li>
+          <li className={styles.li}>
             <NavLink className={styles.a} to='#'>
-          <button className={styles.button}>
-              Tercera Entrega
-          </button>
+              <button className={styles.button}>Tercera Entrega</button>
             </NavLink>
-        </li>
-      </ul>
+          </li>
+        </ul>
       </nav>
     </div>
   )
